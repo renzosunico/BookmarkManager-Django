007cdd8a775ccc20b66ee692a4f2c7cc092bb644
renzosunico <r.sunico@klab.com>
2016-04-13 14:13:51 +0800

    Initial Commit

---
+++ BookmarkManager/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('signup.urls', namespace='signup')),
+    url(r'', include('login.urls', namespace='login')),
+]
